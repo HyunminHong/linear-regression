c2320343dd5b1d7c558a89c82d1664beb9ef7067
Hyunmin Hong <victorius918@gmail.com>
2021-08-08 16:36:01 +0200

    adjusted main.py with a new dataset from sklearn

--- main.py
+++ main.py
@@ -3,7 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.core.fromnumeric import reshape
 import statsmodels.api as sm
+from sklearn import datasets
 
+# Load the diabetes dataset
+diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+
+# Split the data into training/testing sets
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test = diabetes_X[-20:]
+
+# Split the targets into training/testing sets
+diabetes_y_train = diabetes_y[:-20]
+diabetes_y_test = diabetes_y[-20:]
+
+# Train the model using the training sets
+model1 = OLS()
+fitted_mod1 = model1.fit(diabetes_X_train, diabetes_y_train)
+fitted_mod1.beta
+fitted_mod1.rank
+fitted_mod1._dof_model
+fitted_mod1._dof_resid
+diabetes_y_pred = fitted_mod1.predict(diabetes_X_test)
+fitted_mod1.rsquared()
+fitted_mod1.rsquared_adj()
+
+plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
+plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
+plt.show()
+
+"""
 nsample = 100
 x = np.linspace(0, 10, 100)
 X = np.column_stack((x, x**2))
@@ -18,24 +46,6 @@ _X = np.array([[1, 1, 1], [2, 2, 2]])
 X = sm.add_constant(X)
 y = np.dot(X, beta) + e
 
-model1 = OLS()
-fitted_mod1 = model1.fit(X, y)
-fitted_mod1.beta
-fitted_mod1.rank
-fitted_mod1._dof_model
-fitted_mod1._dof_resid
-fitted_mod1.predict(_X)
-fitted_mod1.rss_calc()
-fitted_mod1.rss
-fitted_mod1.tss_calc()
-fitted_mod1.tss 
-fitted_mod1.ess_calc()
-fitted_mod1.ess
-fitted_mod1.rsquared()
-fitted_mod1.rsquared_adj()
-
-# OLS from statsmodels.regression.linear_model
-model2 = sm.OLS(y, X)
-fitted_mod2 = model2.fit()
-fitted_mod2.predict(_X)
-fitted_mod2.summary()
+plt.scatter(X[:,2], y)
+plt.show()
+"""
